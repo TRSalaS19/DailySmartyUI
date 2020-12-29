157d9f3ac97efad52a1dfdfebcf61347d2695e72
Gustavo Salas <gustavosalas@Dis-One.local>
2020-12-29 10:12:01 -0800

    connected recentposts to redux and setup redux thunk

--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
 
-export default class RecentPosts extends Component {
-  render() {
+import { connect } from 'react-redux';
+
+import * as actions from "../actions";
+
+class RecentPosts extends Component {
+
+  componentDidMount(){
+    this.props.fetchRecentPosts();
+  }
+
+  render () {
     return (
       <div className="recent-posts">
-        <div className="recent-posts-wrapper">
-          <div className="recent-posts-heading">Recent Posts</div>
-          <ul className="recent-posts-posts">
-          <li>recent post 0</li>
-          <li>recent post 1</li>
-          <li>recent post 2</li>
-          <li>recent post 3</li>
-          </ul>
-        </div>
+          <div className="recent-posts-wrapper">
+              <div className="recent-posts-heading">Recent Posts</div>
+              <ul className="recent-posts-posts">
+                  <li>recent post 0</li>
+                  <li>recent post 1</li>
+                  <li>recent post 2</li>
+                  <li>recent post 3</li>
+                </ul>
+          </div>
       </div>
-    );
+    )
   }
 }
+export default connect(null, actions)(RecentPosts);
